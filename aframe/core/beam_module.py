50b840e33e892dfce030f7732925601311ec16b1
Nicholas Orndorff <norndorff@live.com>
2023-06-28 12:59:43 -0700

    caddee_core import

--- aframe/core/beam_module.py
+++ aframe/core/beam_module.py
@@ -1,6 +1,6 @@
 from lsdo_modules.module_csdl.module_csdl import ModuleCSDL
 from lsdo_modules.module.module import Module
-from caddee.caddee_core.system_model.design_scenario.design_condition.mechanics_group.mechanics_model.mechanics_model import MechanicsModel
+from caddee.core.caddee_core.system_model.design_scenario.design_condition.mechanics_group.mechanics_model.mechanics_model import MechanicsModel
 from aframe.core.aframe import Aframe
 import numpy as np
 
