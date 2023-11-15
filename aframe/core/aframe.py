90d10c54a4d70687cd7ce7a9af3b77ba55dfad96
Nicholas Orndorff <norndorff@live.com>
2023-11-15 10:49:29 -0800

    improved buckling

--- aframe/core/aframe.py
+++ aframe/core/aframe.py
@@ -453,16 +453,22 @@ class Aframe(ModuleCSDL):
             n = len(beams[beam_name]['nodes'])
             E = beams[beam_name]['E']
             v = 0.33 # Poisson's ratio
-            k = 3.0
+            k = 6.3
+            num_ribs = 12
+
+            length_helper = self.create_output(beam_name + '_length_helper', shape=(n - 1), val=0)
+            for i in range(n - 1):
+                element_name = beam_name + '_element_' + str(i)
+                length_helper[i] = self.declare_variable(element_name + 'L')
+            total_beam_length = csdl.sum(length_helper)
+            length_per_rib = total_beam_length/num_ribs
 
             bkl = self.create_output(beam_name + '_bkl', shape=(n - 1), val=0)
             for i in range(n - 1):
                 element_name = beam_name + '_element_' + str(i)
 
                 wb = self.declare_variable(element_name + '_w')
-                #hb = self.declare_variable(element_name + '_h')
                 tcapb = self.declare_variable(element_name + '_tcap')
-                #a = self.declare_variable(element_name + 'L')
 
                 critical_stress = k*E*(tcapb/wb)**2/(1 - v**2) # Roark's simply-supported panel buckling
                 #self.print_var(critical_stress)
@@ -471,6 +477,9 @@ class Aframe(ModuleCSDL):
                 actual_stress = (actual_stress_array[0] + actual_stress_array[1])/2
 
                 bkl[i] = actual_stress/critical_stress # greater than 1 = bad
+
+            # for i in range(num_ribs - 1): # iterate over the panels
+                
 
 
 
